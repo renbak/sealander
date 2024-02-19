607b99a047e252fc556d510470fc7d630a1841e9
John Rinehart <johnrichardrinehart@gmail.com>
2024-02-18 22:50:51 -0800

    chore: correct spelling of Portimão
    chore: reorder city and country names

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
           <h1 footer-title>AURORA</h1>
           <p className="info">mcaaurora2020@gmail.com</p>
           <p className="info">+351 965 118 884</p>
-          <p className="info">Portugal, Potrimāo</p>
+          <p className="info">Portimāo, Portugal</p>
           <p className="info">+38 093 072 61 57 </p>
-          <p className="info">Ukraine, Odesa</p>
+          <p className="info">Odesa, Ukraine</p>
         </div>
       </div>
     </div>
